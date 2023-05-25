cf6d3cd1245b9959f34e36fae752ddd8b0de829a
Daighter <arkansiel@naver.com>
2023-05-25 17:20:23 +0900

    feat : 스킬 추상클래스 구현

--- Assets/Skills/Skill.cs
+++ Assets/Skills/Skill.cs
@@ -4,4 +4,20 @@ using UnityEngine;
 
 public abstract class Skill : MonoBehaviour
 {
+    protected Rigidbody rb;
+    protected int ap;
+
+    protected virtual void Awake()
+    {
+        ap = GetComponent<Player>().Ap;
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    protected void OnCollisionEnter(Collision collision)
+    {
+        IHitable target = collision.gameObject.GetComponent<IHitable>();
+        if (target != null)
+            target.TakeDamage(ap);
+    }
 }
