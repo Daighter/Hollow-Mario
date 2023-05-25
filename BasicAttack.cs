1ead2dd9c7e72ba3fe40e0e4e417bcb053220a80
Daighter <arkansiel@naver.com>
2023-05-25 21:26:16 +0900

    ?

---
+++ BasicAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttack : Skill
+{
+    [SerializeField] int damage = 2;
+
+    private void Awake()
+    {
+        runTime = 0.1f;
+    }
+
+    private void Start()
+    {
+        base.Awake();
+        skill_ap = player_ap * damage;
+    }
+}
