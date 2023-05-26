c8cb6afa4f4ac408000515c73cdee5245b4dfc80
Daighter <arkansiel@naver.com>
2023-05-26 21:10:14 +0900

    feat : 데이터 매니저에 플레이어 체력정보 추가

--- Assets/Darik/Scripts/Configs/DataManager.cs
+++ Assets/Darik/Scripts/Configs/DataManager.cs
@@ -1,7 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DataManager : MonoBehaviour
 {
+    public UnityEvent<int> OnPlayerHpChanged;
+
+    private int player_hp;
+
+    public void ChangePlayerHp(int player_hp)
+    {
+        OnPlayerHpChanged?.Invoke(player_hp);
+    }
 }
