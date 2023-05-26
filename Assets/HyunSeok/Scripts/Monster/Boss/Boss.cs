08a19c240b5719345b6b7a87decd1740e5d63c53
Geundol222 <geundol222@gmail.com>
2023-05-26 16:43:21 +0900

    feat : 보스...

---
+++ Assets/HyunSeok/Scripts/Monster/Boss/Boss.cs
@@ -0,0 +1,62 @@
+using BossStates;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : Monster
+{
+    [SerializeField] private string name;
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public GameObject crush;
+    [SerializeField] public GameObject normal;
+
+    StateMachine<State, Boss> stateMachine;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        stateMachine = new StateMachine<State, Boss>(this);
+        stateMachine.AddState(State.Idle,           new BossIdleState(this, stateMachine));
+        stateMachine.AddState(State.CrushAttack,    new BossCrushAttackState(this, stateMachine));
+        stateMachine.AddState(State.NormalAttack,   new BossNormalAttackState(this, stateMachine));
+        stateMachine.AddState(State.BombAttack,     new BossBombAttackState(this, stateMachine));
+        stateMachine.AddState(State.Trace,          new BossTraceState(this, stateMachine));
+        stateMachine.AddState(State.Damage,         new BossDamageState(this, stateMachine));
+        stateMachine.AddState(State.Die,            new BossDieState(this, stateMachine));
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        stateMachine.Setup(State.Idle);
+        crush.GetComponent<Collider2D>().enabled = false;
+        normal.GetComponent<Collider2D>().enabled = false;
+    }
+
+    private void Update()
+    {
+        stateMachine.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        Turn();
+    }
+
+    private void Turn()
+    {
+        if (dir.x < 0)
+            render.flipX = true;
+        else if (dir.x > 0)
+            render.flipX = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
---
+++ Assets/HyunSeok/Scripts/Monster/Boss/BossStateList.cs
@@ -0,0 +1,258 @@
+using BossStates;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossIdleState : BossState
+{
+    public BossIdleState(Boss owner, StateMachine<State, Boss> stateMachine) : base(owner, stateMachine) { }
+
+    public override void Setup()
+    {
+        
+    }
+
+    public override void Enter()
+    {
+        attackSeqeunce = Random.Range(0, 3);
+    }
+
+    public override void Update()
+    {
+        dir = (player.position - transform.position).normalized;
+        attackTime += Time.deltaTime;
+    }
+
+    public override void Transition()
+    {
+        if (Vector2.Distance(player.position, transform.position) < attackRange)
+        {
+            if (attackTime > 3f)
+            {
+                attackTime = 0;
+                switch (attackSeqeunce)
+                {
+                    case 0:
+                        anim.SetTrigger("CrushAttack");
+                        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+                        {
+                            stateMachine.ChangeState(State.CrushAttack);
+                        }
+                        break;
+                    case 1:
+                        anim.SetTrigger("NormalAttack");
+                        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+                        {
+                            stateMachine.ChangeState(State.NormalAttack);
+                        }
+                        break;
+                    case 2:
+                        anim.SetTrigger("BombAttack");
+                        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+                        {
+                            stateMachine.ChangeState(State.BombAttack);
+                        }
+                        break;
+                }
+            }            
+        }
+
+        if (Vector2.Distance(player.position, transform.position) < detectRange
+            && Vector2.Distance(player.position, transform.position) > attackRange)
+        {
+            stateMachine.ChangeState(State.Trace);
+        }
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
+
+public class BossCrushAttackState : BossState
+{
+    public BossCrushAttackState(Boss owner, StateMachine<State, Boss> stateMachine) : base(owner, stateMachine) { }
+
+    public override void Setup()
+    {
+        
+    }
+
+    public override void Enter()
+    {
+        crush.transform.position = player.position + new Vector3(0, -1, 0);
+        crush.GetComponent<Collider2D>().enabled = true;
+    }
+
+    public override void Update()
+    {
+        dir = (player.position - transform.position).normalized;
+    }
+
+    public override void Transition()
+    {
+        stateMachine.ChangeState(State.Idle);
+    }
+
+    public override void Exit()
+    {
+        crush.GetComponent<Collider2D>().enabled = false;
+    }
+}
+
+public class BossNormalAttackState : BossState
+{
+    public BossNormalAttackState(Boss owner, StateMachine<State, Boss> stateMachine) : base(owner, stateMachine) { }
+
+    public override void Setup()
+    {
+
+    }
+
+    public override void Enter()
+    {
+        normal.GetComponent<Collider2D>().enabled = true;
+    }
+
+    public override void Update()
+    {
+        dir = (player.position - transform.position).normalized;
+    }
+
+    public override void Transition()
+    {
+        stateMachine.ChangeState(State.Idle);
+    }
+
+    public override void Exit()
+    {
+        normal.GetComponent<Collider2D>().enabled = false;
+    }
+}
+
+public class BossBombAttackState : BossState
+{
+    public BossBombAttackState(Boss owner, StateMachine<State, Boss> stateMachine) : base(owner, stateMachine) { }
+
+    public override void Setup()
+    {
+
+    }
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Update()
+    {
+        dir = (player.position - transform.position).normalized;
+    }
+
+    public override void Transition()
+    {
+        stateMachine.ChangeState(State.Idle);
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
+
+public class BossTraceState : BossState
+{
+    public BossTraceState(Boss owner, StateMachine<State, Boss> stateMachine) : base(owner, stateMachine) { }
+
+    public override void Setup()
+    {
+
+    }
+
+    public override void Enter()
+    {
+        anim.SetBool("IsMove", true);
+        attackSeqeunce = Random.Range(0, 3);
+    }
+
+    public override void Update()
+    {
+        dir = (player.position - transform.position).normalized;
+        transform.Translate(dir * owner.moveSpeed * Time.deltaTime);
+        attackTime += Time.deltaTime;
+    }
+
+    public override void Transition()
+    {
+        if (Vector2.Distance(player.position, transform.position) < attackRange)
+        {
+            stateMachine.ChangeState(State.Idle);
+        }
+    }
+
+    public override void Exit()
+    {
+        anim.SetBool("IsMove", false);
+    }
+}
+
+public class BossDamageState : BossState
+{
+    public BossDamageState(Boss owner, StateMachine<State, Boss> stateMachine) : base(owner, stateMachine) { }
+
+    public override void Setup()
+    {
+
+    }
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Transition()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+public class BossDieState : BossState
+{
+    public BossDieState(Boss owner, StateMachine<State, Boss> stateMachine) : base(owner, stateMachine) { }
+
+    public override void Setup()
+    {
+
+    }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Transition()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
