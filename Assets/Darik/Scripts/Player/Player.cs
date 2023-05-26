d2b48cb7a3f509a3ff48edc1b9593a62dee495ed
Daighter <arkansiel@naver.com>
2023-05-26 19:51:11 +0900

    feat : 플레이어 피격 구현

--- Assets/Darik/Scripts/Player/Player.cs
+++ Assets/Darik/Scripts/Player/Player.cs
@@ -6,11 +6,24 @@ public class Player : HpEntity
 {
     [SerializeField] private int ap = 1;
 
+    private Animator animator;
+
     public int Hp {  get { return hp; } }
     public int Ap { get { return ap; } }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Start()
     {
         hp = 10;
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        animator.SetTrigger("OnHited");
     }
 }
